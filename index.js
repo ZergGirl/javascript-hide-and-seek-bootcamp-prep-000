639173ab6b06f187835d730c5bdf32158d3697d4
Kathy Shin <katchat143@gmail.com>
2016-09-23 02:35:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+function increaseRankBy(n) {
+  const rankedLists= document.querySelectorAll(".ranked-list")
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, p= children.length; j<p; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+}
+}
+function deepestChild() {
+  let node= document.getElementById("grand-node")
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
+  }
